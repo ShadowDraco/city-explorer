a62ff5bc2ca68cc18bc8308962ba9b20709492d2
Ethan Storm <stormethan10@gmail.com>
2023-02-20 15:59:10 -0800

    testing if API token works through client and through netlify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import axios from 'axios'
+
+import Button from 'react-bootstrap/Button'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let LOCATION_ACCESS_TOKEN = process.env.LOCATION_ACCESS_TOKEN
+	let searchString = 'seattle'
+	console.log(process.env.LOCATION_ACCESS_TOKEN)
+
+	let requestLocation = async () => {
+		axios
+			.get(
+				`https://us1.locationiq.com/v1/search?key=${LOCATION_ACCESS_TOKEN}&q=${searchString}&format=json`
+			)
+			.then(res => {
+				console.log(res.data)
+			})
+	}
+
+	return (
+		<div className='App'>
+			<Button onClick={requestLocation}>Click me</Button>
+		</div>
+	)
 }
 
-export default App;
+export default App
