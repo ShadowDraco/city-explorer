41c81315d095f1f9455917ac3d720d2f423d2f4d
Ethan Storm <stormethan10@gmail.com>
2023-02-20 16:15:51 -0800

    fixed problems, ready to deploy and test

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import axios from 'axios'
 import Button from 'react-bootstrap/Button'
 
 function App() {
-	let LOCATION_ACCESS_TOKEN = process.env.LOCATION_ACCESS_TOKEN
+	const ACCESS_TOKEN = process.env.REACT_APP_LOCATION_ACCESS_TOKEN
 	let searchString = 'seattle'
-	console.log(process.env.LOCATION_ACCESS_TOKEN)
 
 	let requestLocation = async () => {
 		axios
 			.get(
-				`https://us1.locationiq.com/v1/search?key=${LOCATION_ACCESS_TOKEN}&q=${searchString}&format=json`
+				`https://us1.locationiq.com/v1/search?key=${ACCESS_TOKEN}&q=${searchString}&format=json`
 			)
 			.then(res => {
 				console.log(res.data)
