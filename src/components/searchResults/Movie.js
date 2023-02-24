1fa438d90515428247b9488ba39dda35bb74bf19
Ethan Storm <stormethan10@gmail.com>
2023-02-23 16:22:24 -0800

    added WeatherDay and Movie Component

---
+++ src/components/searchResults/Movie.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
+
+export default class Movie extends React.Component {
+	constructor(props) {
+		super(props)
+		console.log()
+	}
+
+	render() {
+		return (
+			<Container key={this.props.movie.image_url} className='movie'>
+				<Image src={this.props.movie.image_url} alt={this.props.movie.title} />
+				<p>{this.props.movie.overview}</p>
+			</Container>
+		)
+	}
+}
